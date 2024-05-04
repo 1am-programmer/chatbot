8d50dee709b4db706b0f9c21090032e0b8255af5
Your Name <dannydotdev@gmail.com>
2024-05-04 13:47:49 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    define: {
+      _APP_ENV_: process.env.VITE_VERCEL_ENV,
+    },
+  };
+});
